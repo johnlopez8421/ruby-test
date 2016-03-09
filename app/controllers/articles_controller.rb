b25675261c991fe75218f2506709de06ce5eda63
John Lopez <john.lopez.8421@gmail.com>
2016-03-08 22:05:38 -0800

    add database @ 5.7

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,22 @@
 class ArticlesController < ApplicationController
-  def new
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    @article.save
+    Rails.logger.debug 'DEBUG: '
+    Rails.logger.debug @article
+    redirect_to @article
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
