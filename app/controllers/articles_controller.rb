b81495ac68f2c1af0c510b45b9cbffb0aa1ef9f5
John Lopez <john.lopez.8421@gmail.com>
2016-03-08 22:42:07 -0800

    CRUD complete @ end of 5.x

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,7 +24,7 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
-  
+
   def update
     @article = Article.find(params[:id])
 
@@ -35,6 +35,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
+  end
   private
     def article_params
       params.require(:article).permit(:title, :text)
